7c38e7930884ea8748b5ef93c3f25305f054c948
Lukie Kang <lukiek+f@gmail.com>
2019-05-10 12:10:57 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,6 @@
 def display_rainbow(rainbow)
 
-puts "R: #{rainbow[0]},"
-puts "O: #{rainbow[1]},"
-puts "Y: #{rainbow[2]},"
-puts "G: #{rainbow[3]},"
-puts "B: #{rainbow[4]},"
-puts "I: #{rainbow[5]},"
-puts "V: #{rainbow[6]},"
+puts "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}, G: #{rainbow[3]}, B: #{rainbow[4]}, I: #{rainbow[5]}, V: #{rainbow[6]},"
 end
 
   # Write your #display_rainbow method here
