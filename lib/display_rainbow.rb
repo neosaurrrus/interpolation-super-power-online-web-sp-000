7340ab153f21c02f30ca2fc4a6b0e2469820b27b
Lukie Kang <lukiek+f@gmail.com>
2019-05-10 12:09:00 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,12 @@
-def display_rainbow
+def display_rainbow(rainbow)
 
-puts "R: #{red},"
-puts "O: #{orange},"
-puts "Y: #{yellow},"
-puts "G: #{green},"
-puts "B: #{blue},"
-puts "I: #{indigo},"
-puts "V: #{violet},"
+puts "R: #{rainbow[0]},"
+puts "O: #{rainbow[1]},"
+puts "Y: #{rainbow[2]},"
+puts "G: #{rainbow[3]},"
+puts "B: #{rainbow[4]},"
+puts "I: #{rainbow[5]},"
+puts "V: #{rainbow[6]},"
 end
 
   # Write your #display_rainbow method here
